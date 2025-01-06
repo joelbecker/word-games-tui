6e23525355f317f5ea6fe118614d82c63b5f73cb
Joel Becker <mail@joelbecker.ca>
2025-01-06 16:33:42 -0500

    fix connections parser

--- connections/scrape.py
+++ connections/scrape.py
@@ -8,12 +8,11 @@ import calendar
 import subprocess
 from datetime import datetime
 
+import bs4
+import requests
+
 
 PUZZLE_FILE = os.path.expanduser('~/.wordgames/connections.json')
-
-
-def is_word_list(s):
-    return re.match(r': ([A-Z ]+(,)*){4}', s)
 
 
 def parse_word_list(s):
@@ -21,73 +20,63 @@ def parse_word_list(s):
     return [word.lower() for word in split_words]
 
 
-class ConnectionsParser(html.parser.HTMLParser):
-        def __init__(self):
-            super().__init__()
-            self.in_ul = False
-            self.in_li = False
-            self.ul_contents = []
-            self.li_contents = []
-
-        def handle_starttag(self, tag, attrs):
-            if tag == 'ul':
-                self.in_ul = True
-                self.ul_contents.append([])
-
-        def handle_endtag(self, tag):
-            if tag == 'ul' and self.in_ul:
-                self.in_ul = False
-
-        def handle_data(self, data):
-            if self.in_ul:
-                self.ul_contents[-1].append(data)
+def is_word_category_list(ul):
+    list_items = ul.find_all('li')
+    if len(list_items) != 4:
+        return False
+    return all([len(parse_word_list(li.p.text)) == 4 for li in list_items])
 
 
-def get_connections_puzzle():
-    month = datetime.now().strftime("%B").lower()
-    day = int(datetime.now().strftime("%d"))
-    year = int(datetime.now().strftime("%Y"))
+def get_connections_puzzle(url):
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    
+    html_lists = soup.find_all("ul")
+    word_list = [l for l in html_lists if is_word_category_list(l)][0].find_all('li')
+
+
+    # TODO: Parsing fails for Dec 6th puzzle, likely because of special characters in the category name
+
+    category_names = {
+        'yellow': word_list[0].p.strong.text,
+        'green': word_list[1].p.strong.text,
+        'blue': word_list[2].p.strong.text,
+        'purple': word_list[3].p.strong.text
+    }
+
+    category_words = {
+        'yellow': parse_word_list(word_list[0].p.text),
+        'green': parse_word_list(word_list[1].p.text),
+        'blue': parse_word_list(word_list[2].p.text),
+        'purple': parse_word_list(word_list[3].p.text)
+    }
+    try:
+        assert all(category_names.values()), "Each category must have a name."
+        assert all(category_words.values()), "Each category must have words."
+        for category, words in category_words.items():
+            assert len(words) == 4, f"Category must have exactly 4 words, not {len(words)}. ({category}: {words})"
+    except Exception as e:
+        print(f"Error parsing puzzle for {url}: {e}")
+        raise e
+    
+    return category_names, category_words
+
+def fetch_latest_connections_puzzle(dt=None):
+    if dt is None:
+        dt = datetime.now()
+
+    month = dt.strftime("%B").lower()
+    day = int(dt.strftime("%d"))
+    year = int(dt.strftime("%Y"))
     connections_url = 'https://mashable.com/article/nyt-connections-hint-answer-today-{}-{}'.format(
         month,
         day
     )
     if year >= 2025:
         connections_url += f'-{year}'
-        
-    connections_html = subprocess.run(['curl', connections_url, '>', 'connections.html'], capture_output=True)
+
+    scraped_categories, scraped_words = get_connections_puzzle(connections_url)
     
-    parser = ConnectionsParser()
-    parser.feed(connections_html.stdout.decode('utf-8'))
-
-    html_lists = parser.ul_contents
-    word_list = [html.unescape(l) for l in html_lists if any(is_word_list(s) for s in l)][0]
-
-    # TODO: Parsing fails for Dec 6th puzzle, likely because of special characters in the category name
-
-    categories = {
-        'yellow': word_list[0],
-        'green': word_list[2],
-        'blue': word_list[4],
-        'purple': word_list[6]
-    }
-
-    words= {
-        'yellow': parse_word_list(word_list[1]),
-        'green': parse_word_list(word_list[3]),
-        'blue': parse_word_list(word_list[5]),
-        'purple': parse_word_list(word_list[7])
-    }
-
-    assert all(categories.values()), "Each category must have a name."
-    assert all(words.values()), "Each category must have words."
-    assert all(len(v) == 4 for v in words.values()), "Each category must have exactly 4 words."
-
-    return categories, words
-
-def fetch_latest_connections_puzzle():
-
-    scraped_categories, scraped_words = get_connections_puzzle()
-
     puzzle_data = {
         'date': datetime.now().strftime('%Y-%m-%d'),
         'categories': scraped_categories,
